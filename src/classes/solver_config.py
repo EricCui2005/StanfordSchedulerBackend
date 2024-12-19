3537b61382aa1f799c01a0d82f0c4b496d95aea6
ericcui <ericcui2005@gmail.com>
2024-12-18 19:13:16 -0500

    Comments

--- src/classes/solver_config.py
+++ src/classes/solver_config.py
@@ -27,9 +27,8 @@ class SolverConfig:
         program: Program,
         profile: Profile = None,
     ) -> None:
-        """Initializes the SolverConfig class."""
+        
         self._solver = Solver()
-
         self._program = program
         self._profile = profile
 
@@ -51,27 +50,73 @@ class SolverConfig:
         }
 
         self.update()
-
-    @property
-    def course_dict(self) -> Dict[int, Course]:
-        return self._course_dict
-
-    @property
-    def prereq_graph(self) -> Dict[int, Set[int]]:
-        return self._prereq_graph
+        
+        
     
-    @property
-    def constraints(self) -> Dict[str, List[ExprRef]]:
-        return self._constraints
-    
+    # Solver functions
+    """_summary_
+    Gets the constraints
+    """
     def get_assertions(self) -> List[ExprRef]:
         return self._solver.assertions()
     
+    """_summary_
+    Checks if a schedule is solvable
+    """
     def check_solvable(self) -> CheckSatResult:
         return self._solver.check()
+
+    """_summary_
+    Updates the constraints
+    """
+    def update(self) -> None:
+        self._solver.reset()
+        for func in self._modifiers.values():
+            func()
     
+    """_summary_
+    Adds max quarter units constraints to solver
+    """
+    def _max_quarter_units(self) -> None:
+        for quarter in Quarter:
+            load = sum([If(courseVar == quarter.value, course.units, 0) for courseVar, course in self._course_dict.items()])
+            constraint = load <= self._profile.max_quarter_units
+            self._solver.add(constraint)
+
+    """_summary_
+    Adds min quarter units constraints to solver
+    """
+    def _min_quarter_units(self) -> None:
+        for quarter in Quarter:
+            load = sum([If(courseVar == quarter.value, course.units, 0) for courseVar, course in self._course_dict.items()])
+            constraint = load >= self._profile.min_quarter_units
+            self._solver.add(constraint)
+
+    """_summary_
+    Adds prerequisite constraints to solver
+    """
+    def _prerequisites(self) -> None:
+        for course_code, prereqs in self._prereq_graph.items():
+            course_var = Int(course_code)
+            for prereq_code in prereqs:
+                prereq_var = Int(prereq_code)
+                self._solver.add(course_var > prereq_var)
+
+    """_summary_
+    Adds required courses constraint to solver
+    """
+    def _required_courses(self) -> None:
+        for courseVar, course in self._course_dict.items():
+            constraint = Or([courseVar == quarter.value for quarter in course.offered_quarters])
+            self._solver.add(constraint)
+            
+    def _required_pools(self) -> None:
+        pass
+    
+    """_summary_
+    Solves using all constraints and returns a viable schedule or None if none is possible
+    """
     def solve(self) -> Dict[str, Quarter] | None:
-        """Solves the constraints and returns a schedule."""
         if self.check_solvable() == sat:
             model = self._solver.model()
             schedule = {}
@@ -84,39 +129,17 @@ class SolverConfig:
         else:
             return None
 
-    def update(self) -> None:
-        """Updates the constraints."""
-        self._solver.reset()
-        for func in self._modifiers.values():
-            func()
+
+
+    """Accessors"""
+    @property
+    def course_dict(self) -> Dict[int, Course]:
+        return self._course_dict
+    @property
+    def prereq_graph(self) -> Dict[int, Set[int]]:
+        return self._prereq_graph
+    @property
+    def constraints(self) -> Dict[str, List[ExprRef]]:
+        return self._constraints
     
-    def _max_quarter_units(self) -> None:
-        """Adds max quarter units constraints to solver."""
-        for quarter in Quarter:
-            load = sum([If(courseVar == quarter.value, course.units, 0) for courseVar, course in self._course_dict.items()])
-            constraint = load <= self._profile.max_quarter_units
-            self._solver.add(constraint)
-
-    def _min_quarter_units(self) -> None:
-        """Adds min quarter units constraints to solver."""
-        for quarter in Quarter:
-            load = sum([If(courseVar == quarter.value, course.units, 0) for courseVar, course in self._course_dict.items()])
-            constraint = load >= self._profile.min_quarter_units
-            self._solver.add(constraint)
-
-    def _prerequisites(self) -> None:
-        """Adds prerequisite constraints to solver."""
-        for course_code, prereqs in self._prereq_graph.items():
-            course_var = Int(course_code)
-            for prereq_code in prereqs:
-                prereq_var = Int(prereq_code)
-                self._solver.add(course_var > prereq_var)
-
-    def _required_courses(self) -> None:
-        """Adds required courses constraints to solver."""
-        for courseVar, course in self._course_dict.items():
-            constraint = Or([courseVar == quarter.value for quarter in course.offered_quarters])
-            self._solver.add(constraint)
-            
-    def _required_pools(self) -> None:
-        pass
+    
